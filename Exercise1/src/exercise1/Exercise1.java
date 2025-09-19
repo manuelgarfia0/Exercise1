8e63777b029f2f5b88667fa5e1fca30cdaae05d4
manuel.garfia <manuel.garfia@106-10.info2>
2025-09-19 12:22:11 +0200

    Exercise 1 finished

--- Exercise1/src/exercise1/Exercise1.java
+++ Exercise1/src/exercise1/Exercise1.java
@@ -3,7 +3,10 @@ package exercise1;
 public class Exercise1 {
 
 	public static void main(String[] args) {
-
+		
+		System.out.println("Hello, who are you?");
+		System.out.println("Fine thanks.");
+		
 	}
 
 }
